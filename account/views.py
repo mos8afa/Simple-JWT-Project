4f15b74ef87c49a1692f3a72eb82a4912e224c55
Mostafa Mohamed <imos8afa@gmail.com>
2026-02-05 15:32:27 +0200

    Setup JWT authentication and profile API

---
+++ account/views.py
@@ -0,0 +1,12 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view , permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def login(request):
+    return Response({
+    "username": request.user.username,
+    "email": request.user.email
+})
+
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_simplejwt',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+
+    ]
+}
 
 ROOT_URLCONF = 'project.urls'
 
